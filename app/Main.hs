69cda03d27e1ea2a30d339abd928767880b69a6c
Benjamin <benjamin.guy.thomas@gmail.com>
2022-06-05 15:54:00 +0200

    Display notes/server state

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main where
+module Main (main) where
 
-import Lucid (h1_, p_)
-import Web.Spock (SpockM, get, root, runSpock, spock)
+import Control.Monad (forM_)
+import Control.Monad.IO.Class (liftIO)
+import Data.IORef (IORef, newIORef, readIORef)
+import Data.Text (Text)
+import Lucid (ToHtml (toHtml), h1_, li_, ul_)
+import Web.Spock (HasSpock (getState), SpockM, get, root, runSpock, spock)
 import Web.Spock.Config (
     PoolOrConn (PCNoDatabase),
     defaultSpockCfg,
@@ -15,15 +19,34 @@ import Web.Spock.Lucid (lucid)
 -- Run with:
 --  ghcid -T :main
 
-type Server a = SpockM () () () a
+type Server a = SpockM () () ServerState a
+
+data Note = Note
+    { author :: Text
+    , contents :: Text
+    }
+
+newtype ServerState = ServerState {notes :: IORef [Note]}
 
 app :: Server ()
-app = get root $
+app = get root $ do
+    notes' <- getState >>= (liftIO . readIORef . notes)
     lucid $ do
-        h1_ "Lucid templates..."
-        p_ "Are awesome!!"
+        h1_ "Notes"
+        ul_ $
+            forM_ notes' $ \note -> li_ $ do
+                toHtml (author note)
+                ": "
+                toHtml (contents note)
 
 main :: IO ()
 main = do
-    cfg <- defaultSpockCfg () PCNoDatabase ()
+    state <-
+        ServerState
+            <$> newIORef
+                [ Note "Bob" "Learn some Haskell"
+                , Note "John" "Keep learning Haskell"
+                ]
+
+    cfg <- defaultSpockCfg () PCNoDatabase state
     runSpock 8080 (spock cfg app)

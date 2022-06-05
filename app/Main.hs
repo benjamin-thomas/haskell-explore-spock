ceb88ffc8dddf990e5980bf8cfc27c183680818b
Benjamin <benjamin.guy.thomas@gmail.com>
2022-06-05 15:21:55 +0200

    Basic server running

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Web.Spock
+import Web.Spock.Config
+
+-- https://haskell-at-work.com/episodes/2018-04-09-your-first-web-application-with-spock.html
+--
+-- Run with:
+--  ghcid -T :main
+
+type Server a = SpockM () () () a
+
+app :: Server ()
+app = get root (html "<h1>Hello!!</h1>")
+
 main :: IO ()
-main = putStrLn "Hello, Haskell2!"
+main = do
+    cfg <- defaultSpockCfg () PCNoDatabase ()
+    runSpock 8080 (spock cfg app)

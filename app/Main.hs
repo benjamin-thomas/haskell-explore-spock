3b0279fd37fe5174be08955b310cebdb59c61331
Benjamin <benjamin.guy.thomas@gmail.com>
2022-06-05 16:06:45 +0200

    Add post handler

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,17 @@ module Main (main) where
 
 import Control.Monad (forM_)
 import Control.Monad.IO.Class (liftIO)
-import Data.IORef (IORef, newIORef, readIORef)
+import Data.IORef (IORef, atomicModifyIORef', newIORef, readIORef)
 import Data.Text (Text)
 import Lucid (ToHtml (toHtml), h1_, li_, ul_)
-import Web.Spock (HasSpock (getState), SpockM, get, root, runSpock, spock)
+import Web.Spock (HasSpock (getState), SpockM, get, param', post, redirect, root, runSpock, spock)
 import Web.Spock.Config (
     PoolOrConn (PCNoDatabase),
     defaultSpockCfg,
  )
 import Web.Spock.Lucid (lucid)
+
+-- import Data.Semigroup ((<>))
 
 -- https://haskell-at-work.com/episodes/2018-04-09-your-first-web-application-with-spock.html
 --
@@ -29,15 +31,24 @@ data Note = Note
 newtype ServerState = ServerState {notes :: IORef [Note]}
 
 app :: Server ()
-app = get root $ do
-    notes' <- getState >>= (liftIO . readIORef . notes)
-    lucid $ do
-        h1_ "Notes"
-        ul_ $
-            forM_ notes' $ \note -> li_ $ do
-                toHtml (author note)
-                ": "
-                toHtml (contents note)
+app = do
+    get root $ do
+        notes' <- getState >>= (liftIO . readIORef . notes)
+        lucid $ do
+            h1_ "Notes"
+            ul_ $
+                forM_ notes' $ \note -> li_ $ do
+                    toHtml (author note)
+                    ": "
+                    toHtml (contents note)
+    post root $ do
+        newAuthor <- param' "author"
+        newContents <- param' "contents"
+        notesRef <- notes <$> getState
+        liftIO $
+            atomicModifyIORef' notesRef $ \notes_ ->
+                (notes_ <> [Note newAuthor newContents], ())
+        redirect "/"
 
 main :: IO ()
 main = do
